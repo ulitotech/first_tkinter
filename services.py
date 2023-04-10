387114b3b396e1d809d3909b5cd02adb2e621d5f
ulito <ulitotech@gmail.com>
2023-04-10 13:24:52 +0300

    Добавлено распределение строк по листам

--- services.py
+++ services.py
@@ -1,11 +1,13 @@
 import openpyxl as opxl
-from openpyxl.styles import Font, Alignment, Border, Side
+from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
 from xls2xlsx import XLS2XLSX
 import os
 from pathlib import Path
 import pandas as pd
 from typing import Literal
 from tkinter import messagebox as mbox
+
+len_zisu =225
 
 def get_file_path(root:str)->dict:
     """Получение словаря с расположениями рабочих файлов
@@ -70,11 +72,11 @@ def getting_data(path:dict)->list:
     result = pd.merge(result, df_codes, on = "code", how = "left")
     result = result[["serials",339,360,"code","name"]]
     result.drop_duplicates(subset=['serials'],inplace=True)
-    result.fillna("нет_данных", inplace=True)
+    result.fillna("нет данных", inplace=True)
     return result.values.tolist()
 # getting_data(get_file_path(WORKING_FOLDER))
 
-def formating_cell(cell, style:Literal["header"]|Literal["usual"]):
+def formating_cell(cell, style:Literal["header"]|Literal["usual"]|Literal["main"]):
     """Форматирование ячейки"""
     if style=="header":
         font = Font(size=15, name="Times New Roman", bold=True)
@@ -90,19 +92,24 @@ def formating_cell(cell, style:Literal["header"]|Literal["usual"]):
                         right=Side(style='thin'),
                         top=Side(style='thin'),
                         bottom=Side(style='thin'))
+    elif style == "main":
+        font = Font(size=12, name="Times New Roman", bold=False)
+        alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
     cell.font = font   
     cell.alignment = alignment  
-    cell.border = border  
+    if style != "main":
+        cell.border = border  
 
 def creating_header(path:str):
     with open(path["FOR_WORK"], "rb") as main_xl:
         main_wb =  opxl.load_workbook(main_xl, read_only=False)   
         main_sheet = main_wb.active
         header_temp = {23:"Заводской номер прибора учета",24:"1-Величина максимальной мощности; кВт",
-                        25:"2-Величина максимальной мощности; кВт",26:"код РЭСа",27:"Наименование РЭС"}
-        abc_cell = ("W", "X", "Y", "Z", "AA")
+                        25:"2-Величина максимальной мощности; кВт",26:"код РЭСа",27:"Наименование РЭС",
+                        28:"Сравнение"}
+        abc_cell = ("W", "X", "Y", "Z", "AA", "AB")
         
-        for i in range(5):
+        for i in range(6):
             main_sheet.column_dimensions[f"{abc_cell[i]}"].width = 18.9
             main_sheet.merge_cells(f"{abc_cell[i]}11:{abc_cell[i]}12")
             main_sheet.cell(row=11, column=23+i).value = header_temp[23+i]
@@ -115,31 +122,131 @@ def creating_header(path:str):
 
 def adding_data_to_main(path):
     zisu_data = getting_data(path)
-    abc = ("W", "X", "Y", "Z", "AA")
+    
+    global len_zisu
+    len_zisu = len(zisu_data)
+    
+    abc = ("W", "X", "Y", "Z", "AA", "AB")
     with open(path["FOR_WORK"], "rb") as main_xl:
         main_wb =  opxl.load_workbook(main_xl, read_only=False)   
         main_sheet = main_wb.active
         i = 0
+        
+        merged_list = [str(cell) for cell in main_sheet.merged_cells]
+        val = None
+        start = end = -1
         for row in zisu_data:
             for j in range(5):
                 main_sheet[f"{abc[j]}{14+i}"] = row[j]
                 formating_cell(main_sheet[f"{abc[j]}{14+i}"], style="usual")
+                
+            if "Ярэнерго" in main_sheet[f"V{14+i}"].value:
+                if main_sheet[f"R{14+i}"].coordinate in main_sheet.merged_cells:
+                    val = main_sheet[f"R{14+i}"].value
+                    for m_l in merged_list:
+                        if main_sheet[f"R{14+i}"].coordinate == m_l.split(":")[0]:
+                            start = int(m_l.split(":")[0][1:])
+                            end = int(m_l.split(":")[1][1:])
+                            main_sheet.unmerge_cells(m_l)
+                if start <= 14+i <= end:
+                    main_sheet[f"R{14+i}"].value = val
+                formating_cell(main_sheet[f"R{14+i}"], style="main")  
+                    
+                if main_sheet[f"{abc[1]}{14+i}"].value == 0:
+                    main_sheet[f"{abc[1]}{14+i}"].value = main_sheet[f"{abc[2]}{14+i}"].value
+                    
+                if main_sheet[f"R{14+i}"].value != main_sheet[f"{abc[1]}{14+i}"].value:
+                    main_sheet[f"R{14+i}"].fill = PatternFill("solid", fgColor="9400D3")
+                    main_sheet[f"{abc[1]}{14+i}"].fill = PatternFill("solid", fgColor="9400D3")
+                    main_sheet[f"{abc[-1]}{14+i}"].value = "Не равно"
+                else:
+                    main_sheet[f"{abc[-1]}{14+i}"].value = "Равно"
+    
+            formating_cell(main_sheet[f"{abc[-1]}{14+i}"], style="usual")
             i += 1
         main_wb.save(path["FOR_WORK"])
+        
+#adding_data_to_main(get_file_path(WORKING_FOLDER))
+
+def copy_header(workbook:opxl.Workbook, mode:str):
+    sheet_to_copy = workbook["Лист1"]
+    new_sheet = workbook.copy_worksheet(sheet_to_copy)
+    if mode == "no_data":
+        new_sheet.title = "no_data"
+        workbook["no_data"].delete_rows(14,workbook["no_data"].max_row)
+    else:
+        new_sheet.title = mode
+        workbook[mode].delete_rows(14,workbook[mode].max_row)
+    
+def no_data(path):
+    with open(path["FOR_WORK"], "rb") as main_xl:
+        main_wb =  opxl.load_workbook(main_xl, read_only=False)   
+        main_sheet = main_wb["Лист1"]
+        flag = True
+        global len_zisu
+        r = 14
+        for row in main_sheet.iter_rows(min_row = 13, max_row = 14+len_zisu, min_col = 1, max_col=28):
+            if row[-2].value == "нет данных" and "Ярэнерго" in row[-7].value:
+                if flag:
+        
+                    copy_header(main_wb, "no_data")
+                    no_data_sheet = main_wb["no_data"]
+                    
+                    for i in range(len(row)):
+                        no_data_sheet.cell(row=r, column=1+i).value = row[i].value
+                        if row[i].has_style:
+                            no_data_sheet.cell(row=r, column=1+i)._style = row[i]._style
+                    flag = False
+                    
+                else:
+                    no_data_sheet = main_wb["no_data"]
+                    for i in range(len(row)):
+                        no_data_sheet.cell(row=r, column=1+i).value = row[i].value
+                        if row[i].has_style:
+                            no_data_sheet.cell(row=r, column=1+i)._style = row[i]._style
+                r += 1
+        main_wb.save(path["FOR_WORK"])        
+                
+def different(path):
+    with open(path["FOR_WORK"], "rb") as main_xl:
+        main_wb =  opxl.load_workbook(main_xl, read_only=False)   
+        main_sheet = main_wb["Лист1"]
+        global len_zisu
+        for row in main_sheet.iter_rows(min_row = 13, max_row = 13+len_zisu, min_col = 1, max_col=28):
+            if row[-2].value != "нет данных" and "Ярэнерго" in row[-7].value:
+                if row[-2].value not in main_wb.sheetnames:
+                    copy_header(main_wb, row[-2].value)
+                    diff_sheet = main_wb[str(row[-2].value)]
+                    max_row = diff_sheet.max_row+1
+                    for i in range(len(row)):
+                        diff_sheet.cell(row=max_row, column=1+i).value = row[i].value
+                        if row[i].has_style:
+                            diff_sheet.cell(row=max_row, column=1+i)._style = row[i]._style
+                else:
+                    diff_sheet = main_wb[row[-2].value]
+                    max_row = diff_sheet.max_row+1
+                    for i in range(len(row)):
+                        diff_sheet.cell(row=max_row, column=1+i).value = row[i].value
+                        if row[i].has_style:
+                            diff_sheet.cell(row=max_row, column=1+i)._style = row[i]._style
+        main_wb.save(path["FOR_WORK"]) 
+        
     new_path = str(path["FOR_WORK"]).split("7-40")[0]+"7-40_ГОТОВО.xlsx"
     os.rename(path["FOR_WORK"], new_path)
 
-#adding_data_to_main(get_file_path(WORKING_FOLDER))
+
 
 def main_process(path:str, dial, win):
     """Основная рабочая функция, которая запускается после нажатия кнопки""" 
-
     try:
         converting_xls_to_xlsx(str(get_file_path(path)["MAIN"]))
         creating_header(get_file_path(path))
         adding_data_to_main(get_file_path(path))
+        no_data(get_file_path(path))
+        different(get_file_path(path))
         mbox.showinfo("INFO", "Документ собран!!!")
         dial.del_param()
     except Exception as err:
-        pass
+        print(err)
 # main_process(WORKING_FOLDER)
+
